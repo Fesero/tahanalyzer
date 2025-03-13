d9745e09f42026a7517b9696a267f6237b5c6a1a
Fesero <fesero@bk.ru>
2025-03-14 00:35:41 +0300

    v0.1.2.5

---
+++ src/ComposerPlugin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Fesero\Tahanalyzer;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+
+class ComposerPlugin implements PluginInterface, EventSubscriberInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'post-install-cmd' => 'handleEvent',
+            'post-update-cmd' => 'handleEvent'
+        ];
+    }
+
+    public static function handleEvent(Event $event)
+    {
+        // Автоматический запуск при установке/обновлении
+        Installer::copyConfig();
+    }
+}
